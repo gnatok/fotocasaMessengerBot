3169deaa782f13b66b8df98e30ccb010fff3c807
Oleksandr Gnatsyhyn <gnat86@gmail.com>
2016-12-09 22:35:14 +0100

    card added

--- index.js
+++ index.js
@@ -50,15 +50,72 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 });
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text };
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging;
+
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+            let text = event.message.text;
+            if (text === 'Generic') {
+                sendGenericMessage(sender);
+                continue
+            }
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+
+        if (event.postback) {
+            let text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            continue
+        }
+    }
+    res.sendStatus(200)
+});
+
+
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Nice House - 500$",
+                    "subtitle": "Santa Coloma, friendly neighbors",
+                    "image_url": "http://bloodygoodhorror.com/bgh/files/promos/Scary-Haunted-House.jpg",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "Open at fotocasa"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Beautiful House - 800$",
+                    "subtitle": "Gitano Plaza",
+                    "image_url": "https://s-media-cache-ak0.pinimg.com/originals/07/83/05/078305846fde92c1818ea523dad82940.jpg",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    };
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
         method: 'POST',
         json: {
             recipient: {id:sender},
-            message: messageData
+            message: messageData,
         }
     }, function(error, response, body) {
         if (error) {
